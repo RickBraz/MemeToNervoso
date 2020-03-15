f083e22b27b7804dacba6b5f66383c128a919fa6
RickBraz <riicardobraz@gmail.com>
2020-03-14 22:27:43 -0300

    adicionei o programa

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MemeToNervoso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Usuário, o que você pensa sobre mim?");
+
+            string mensagem = Console.ReadLine();
+            string ToNervoso = mensagem.Remove(10) + "... Não, Pera.";
+            Console.WriteLine("E sabe o que eu penso de você?");
+            Console.WriteLine(ToNervoso);
+            Console.WriteLine("Tô nervoso");
+
+        }
+    }
+}
